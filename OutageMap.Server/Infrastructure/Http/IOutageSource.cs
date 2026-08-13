edf7f2244c3836bd7f3f5ee4c745b930673cdcaf
Calderon, Raymond <rcalder7@CougarNet.UH.EDU>
2026-08-12 19:29:14 -0500

    change IOutageService to IOutageSource and implementing files accordingly

---
+++ OutageMap.Server/Infrastructure/Http/IOutageSource.cs
@@ -0,0 +1,8 @@
+﻿using OutageMap.Server.Dtos;
+
+namespace OutageMap.Server.Infrastructure.Http;
+
+    public interface IOutageSource
+    {
+        Task<List<OutageDto>> GetOutageData();
+    }
---
+++ OutageMap.Server/Infrastructure/Http/PollOutageSource.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using OutageMap.Server.Dtos;
+using System.Text.Json;
+
+namespace OutageMap.Server.Infrastructure.Http;
+
+public class PollOutageSource : IOutageSource
+{
+    private readonly HttpClient _httpClient;
+
+    public PollOutageSource(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<OutageDto>> GetOutageData()
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress);
+
+        if (response.IsSuccessStatusCode == false)
+        {
+            var statusCode = (int)response.StatusCode;
+            var codeDescription = ReasonPhrases.GetReasonPhrase(statusCode);
+
+            throw new HttpRequestException($"HTTP Error {statusCode}: {codeDescription}");
+        }
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        var dataDto = JsonSerializer.Deserialize<List<OutageDto>>(json);
+
+        return dataDto;
+    }
+}
