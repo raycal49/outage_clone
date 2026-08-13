21ed45617048df07977692a213c4b25c21cc7b1d
Calderon, Raymond <rcalder7@CougarNet.UH.EDU>
2026-08-12 19:27:17 -0500

    change OutageDataDto to OutageDto and add OutagePoller

---
+++ OutageMap.Server/Dtos/OutageDto.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OutageMap.Server.Dtos;
+
+public class OutageDto
+{
+    [JsonPropertyName("latitude")]
+    public double Latitude { get; set; }
+
+    [JsonPropertyName("longitude")]
+    public double Longitude { get; set; }
+
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("startTime")]
+    public long StartTime { get; set; }
+
+    [JsonPropertyName("lastUpdatedTime")]
+    public long LastUpdatedTime { get; set; }
+
+    [JsonPropertyName("etrTime")]
+    public long? EtrTime { get; set; }
+
+    [JsonPropertyName("numPeople")]
+    public int NumPeople { get; set; }
+
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    [JsonPropertyName("cause")]
+    public string? Cause { get; set; }
+
+    [JsonPropertyName("identifier")]
+    public string? Identifier { get; set; }
+
+    [JsonPropertyName("additionalProperties")]
+    public List<Properties>? AdditionalProperties { get; set; }
+
+}
+
+public sealed class Properties
+{
+    [JsonPropertyName("property")]
+    public string? Property { get; init; }
+
+    [JsonPropertyName("value")]
+    public JsonElement Value { get; init; }
+
+    public string? GetFirstString()
+    {
+        if (Value.GetArrayLength() == 0)
+            throw new InvalidOperationException($"Property '{Property}' had an empty value array.");
+
+        return Value[0].GetString();
+    }
+}
---
+++ OutageMap.Server/Services/BackgroundServices/OutagePoller.cs
@@ -0,0 +1,59 @@
+﻿using OutageMap.Server.Dtos;
+using OutageMap.Server.Infrastructure.Http;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Services.BackgroundServices;
+
+public class OutagePoller : BackgroundService
+{
+    private readonly TimeSpan _period = TimeSpan.FromMinutes(10);
+    private readonly ILogger<OutagePoller> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public OutagePoller(ILogger<OutagePoller> logger, IServiceProvider service)
+    {
+        _logger = logger;
+        _serviceProvider = service;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using PeriodicTimer timer = new(_period);
+
+        int timesPolled = 0;
+
+        _logger.LogInformation("Starting outage polling");
+
+        // this line actually yields List<OutageDto> and so we should capture it, examine it, and compare it with what we have in our EF Core db
+        await PollAsync(++timesPolled);
+
+        while (await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            await PollAsync(++timesPolled);
+        }
+    }
+
+    private async Task PollAsync(int timesPolled)
+    {
+        using IServiceScope scope = _serviceProvider.CreateScope();
+
+        IOutageSource source =
+            scope.ServiceProvider.GetRequiredService<IOutageSource>();
+
+        List<OutageDto> data = await source.GetOutageData();
+
+        _logger.LogInformation(
+            "Poll #{TimesPolled} at {Timestamp}. Outages: {Count}\n{Data}",
+            timesPolled,
+            DateTimeOffset.Now,
+            data.Count,
+            JsonSerializer.Serialize(
+                data,
+                new JsonSerializerOptions { WriteIndented = true }));
+    }
+
+    // CheckIfNewOutagePoints
+
+    // StoreNewOutagePoints
+}
