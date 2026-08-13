6da64d278105791a6f8cebeee7a042b594db0d0d
Calderon, Raymond <rcalder7@CougarNet.UH.EDU>
2026-08-12 19:24:06 -0500

    prune unnecessary attributes on outage entity

---
+++ OutageMap.Server/Models/OutageEntity.cs
@@ -0,0 +1,27 @@
+﻿using NetTopologySuite.Geometries;
+
+public sealed class OutageEntity
+{
+    public long Id { get; set; }
+    public long SourceId { get; set; }
+
+    public DateTimeOffset StartTime { get; set; }
+    public DateTimeOffset? EstimatedRestorationTime { get; set; }
+
+    public int CustomersAffected { get; set; }
+    public string Status { get; set; } = null!;
+    public string? Cause { get; set; }
+
+    public Point Location { get; set; } = null!;
+
+    public string? City { get; set; }
+    public string? County { get; set; }
+    public string? ServiceArea { get; set; }
+    public string? ZipCode { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTimeOffset FirstSeenAt { get; set; }
+    public DateTimeOffset LastChangedAt { get; set; }
+    public DateTimeOffset? ResolvedAt { get; set; }
+}
